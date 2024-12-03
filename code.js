f3ee4267b4fb2b03c9448665c7d4cf13de390577
Mgoodno1 <90008902+Mgoodno1@users.noreply.github.com>
2024-12-02 21:56:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,9 +6,11 @@ function binarySearch(list, element) {
         let middle = Math.floor((left + right) / 2);
 
         if (list[middle] === element) {
-            return middle; 
-        }
-        if (list[middle] < element) {
+            if (middle === 0 || list[middle - 1] !== element) {
+                return middle;
+            }
+            right = middle - 1;
+        } else if (list[middle] < element) {
             left = middle + 1;
         } else {
             right = middle - 1;
